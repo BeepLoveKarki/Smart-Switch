78f05c278c0e888a9a510bcd91292888154093a8
Biplab <biplabkarki04@gmail.com>
2018-05-19 14:32:06 +0545

    New changes

--- models/model.js
+++ models/model.js
@@ -1,6 +1,6 @@
 let MongoClient=require('mongodb').MongoClient;
-//let url="mongodb://beeplove:Meriaama12@ds113200.mlab.com:13200/users";
-let url="mongodb://127.0.0.1:27017/";
+let url="mongodb://beeplove:Meriaama12@ds113200.mlab.com:13200/users";
+//let url="mongodb://127.0.0.1:27017/";
 let nodemailer=require('nodemailer');
 let datas,wifiname;
 
